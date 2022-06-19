03c7002e9b4505f45c2d7da4c9bbccd5dc5eecf7
Aaron Noe <aarminoe@gmail.com>
2022-06-19 12:24:04 -0500

    commit tree 00c7fc6543135de327ac297a83e976e14e9b8c8d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        split_word = @word.split('')
+        anagram = []
+        word_array.each do |i|
+            letter_array = i.split('')
+            if split_word.sort == letter_array.sort
+                anagram << letter_array.join
+            end
+        end
+        anagram
+    end
+end
